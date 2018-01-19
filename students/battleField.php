4bac7103b9a495b064ce7db61b94486511402e39
Axel Dujardin <axel.dujardin@Digital-Grenoble.local>
2018-01-19 17:31:12 +0100

    Upload Game Warrior

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,19 @@
 
   class BattleField extends BaseBattleField
   {
+      public static function createMyWarrior() {
 
+        $Chuck = new MarvelWarrior($GLOBALS['warriorID']);
+        $Chuck->SetImageUrl('http://why-ed.com/wp-content/uploads/2017/04/Chuck-Norris.jpg');
 
+        $ChuckWeapon = new Weapon ("Chuck call your mom");
+        $ChuckWeapon->SetWeapon("https://pbs.twimg.com/media/B4_zKH2IcAA_I_u.jpg");
+
+        $Chuck->SetPower();
+        $Chuck->SetWeapon($ChuckWeapon);
+        $Chuck->saveNew();
+      }
+      public static function createOtherWarrior() {
+
+      }
   }
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,72 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'Radical';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
 
-};
+    public function __construct($newId){
+        $this->id = $newId;
+        $this->speed=30;
+        $this->life=100;
+        $this->shield=20;
+    }
+
+    public function SetWeapon($myWeapon) {
+        $this->weapon = $myWeapon;
+    }
+    public function SetImageUrl($url){
+        $this->imageUrl = $url;
+    }
+}
+
+class StartrekWarrior extends Warrior {
+    public $mentalPower;
+    public function power(){
+        return $this->mentalPower;
+    }
+    public function __construct($newId){
+        parent::__construct($newId);
+        $this->mentalPower=8;
+    }
+}
+class MarvelWarrior extends Warrior {
+    public $superPower;
+    public function power(){
+        return $this->superPower;
+    }
+    public function __construct($newId){
+        parent::__construct($newId);
+        $this->superPower=100;
+    }
+}
+class PokemonWarrior extends Warrior {
+    public $level;
+    public function power(){
+        return $this->level;
+    }
+    public function __construct($newId){
+        parent::__construct($newId);
+        $this->level=1;
+    }
+}
+class Weapon {
+    public $id;
+    public $strength;
+    public $imageUrl;
+    public function __construct($WeaponId,$WeaponStrength){
+        $this->id = $WeaponId;
+        $this->strength = $WeaponStrength;
+    }
+    public function SetImageUrl($url){
+        $this->imageUrl = $url;
+    }
+}
